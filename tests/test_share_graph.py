cc10d898df3780b4927dadf21bb3cccc3bfafd4e
LB <fiffigini@gmail.com>
2025-04-07 20:47:26 +0200

    ✅ removing active=true and adding circular relationship with negative depth test.

--- tests/test_share_graph.py
+++ tests/test_share_graph.py
@@ -18,7 +18,6 @@ from typing import NamedTuple
 class TestInput(NamedTuple):
     shares: list[Share]
     entity_queried: Entity
-    entity_in_focus: Entity
     expected_real_share_amount: ShareAmount
 
 
@@ -36,99 +35,111 @@ class TestShareGraph:
     e_2 = create_test_entity(-2)
 
     @pytest.mark.parametrize(
-        "shares, entity_queried, entity_in_focus, expected_real_share_amount",
+        "shares, entity_queried, expected_real_share_amount",
         [
             # Single 1.0 share
             # Then: 1.0
             TestInput(
-                shares=[Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0), active=True)],
+                shares=[Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0))],
                 entity_queried=e1,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(1.0)),
 
             # Given: Single 0.5 share
             # Then:  0.5
             TestInput(
-                shares=[Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5), active=True)],
+                shares=[Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5))],
                 entity_queried=e1,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.5)),
 
             # Given: two 0.5 shares in serial
             # Then:  0.25
             TestInput(
                 shares=[
-                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5), active=True)],
+                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5))],
                 entity_queried=e2,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.25)),
 
             # Given: three 0.5 shares in serial
             # Then:  0.125
             TestInput(
                 shares=[
-                    Share(source=e3, source_depth=Depth(3), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5), active=True)],
+                    Share(source=e3, source_depth=Depth(3), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5))],
                 entity_queried=e3,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.125)),
 
             # Given: three 0.5 shares, 1 direct and 2 in series to focus
             # Then:  0.75
             TestInput(
                 shares=[
-                    Share(source=e3, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e3, source_depth=Depth(2), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5), active=True)],
+                    Share(source=e3, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e3, source_depth=Depth(2), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(0.5))],
                 entity_queried=e3,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.25+0.125)),
 
             # Given: single negative_depth target with 0.5 shares
             # Then:  0.5
             TestInput(
-                shares=[Share(source=e0, source_depth=Depth(1), target=e_1, target_depth=Depth(-1), amount=ShareAmount.from_exact(0.5), active=True)],
+                shares=[Share(source=e0, source_depth=Depth(0), target=e_1, target_depth=Depth(-1), amount=ShareAmount.from_exact(0.5))],
                 entity_queried=e_1,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.50)),
 
             # Given: two negative_depth target with 0.5 shares
             # Then:  0.25
             TestInput(
                 shares=[
-                    Share(source=e0, source_depth=Depth(0), target=e_1, target_depth=Depth(-1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e_1, source_depth=Depth(-1), target=e_2, target_depth=Depth(-2), amount=ShareAmount.from_exact(0.5), active=True)],
+                    Share(source=e0, source_depth=Depth(0), target=e_1, target_depth=Depth(-1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e_1, source_depth=Depth(-1), target=e_2, target_depth=Depth(-2), amount=ShareAmount.from_exact(0.5))],
                 entity_queried=e_2,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.25)),
 
-            # Given: three 0.5 shares in serial
-            # Then:  0.125
+            # Given: three shares, where last is circular and query for depth 2
+            # Then:  ignores circular path and returns 0.5
             TestInput(
                 shares=[
-                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.1), active=True)],
+                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0)),
+                    Share(source=e1, source_depth=Depth(3), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.1))],
                 entity_queried=e2,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(0.5)),
 
+            # Given: three shares, where last is circular and query for depth 1
+            # Then:  ignores circular path and returns 1.0
             TestInput(
                 shares=[
-                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0), active=True),
-                    Share(source=e1, source_depth=Depth(1), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.1), active=True)],
+                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0)),
+                    Share(source=e1, source_depth=Depth(3), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.1))],
                 entity_queried=e1,
-                entity_in_focus=e0,
                 expected_real_share_amount=ShareAmount.from_exact(1.0)),
 
+            # Given: four shares, where in two level circular path and query for depth 2
+            # Then:  ignores circular path and returns direct path 0.5*1.0 = 0.5
+            TestInput(
+                shares=[
+                    Share(source=e2, source_depth=Depth(2), target=e1, target_depth=Depth(1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e1, source_depth=Depth(1), target=e0, target_depth=Depth(0), amount=ShareAmount.from_exact(1.0)),
+                    Share(source=e1, source_depth=Depth(4), target=e3, target_depth=Depth(3), amount=ShareAmount.from_exact(0.1)),
+                    Share(source=e3, source_depth=Depth(3), target=e2, target_depth=Depth(2), amount=ShareAmount.from_exact(0.1))],
+                entity_queried=e2,
+                expected_real_share_amount=ShareAmount.from_exact(0.5)),
 
-            # TODO test case for forked negative depth query
-            # TODO test with circular negative branching path
-            # TODO Setup test graph statically, then simply query it in the test cases. Much less setup. Probably as swapable test fixtures.
+            # Given: three shares, where last is circular and query for depth -2
+            # Then:  ignores circular path and returns 0.25
+            TestInput(
+                shares=[
+                    Share(source=e0, source_depth=Depth(0), target=e_1, target_depth=Depth(-1), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e_1, source_depth=Depth(-1), target=e_2, target_depth=Depth(-2), amount=ShareAmount.from_exact(0.5)),
+                    Share(source=e_2, source_depth=Depth(-3), target=e_2, target_depth=Depth(-1), amount=ShareAmount.from_exact(0.1))],
+                entity_queried=e_2,
+                expected_real_share_amount=ShareAmount.from_exact(0.25)),
+
+
+
             # TODO test that lower and upper bounds are calculated correctly.
             # TODO test calculations of average.. through a complicated graph.
             # TODO test other share values than 0.5.
@@ -138,14 +149,13 @@ class TestShareGraph:
             self,
             shares: list[Share],
             entity_queried: Entity,
-            entity_in_focus: Entity,
             expected_real_share_amount: ShareAmount):
 
         # Given
-        graph = ShareGraphSparseDictImpl.create_from(shares, max_cycle_iterations=2)
+        graph = ShareGraphSparseDictImpl.create_from(shares)
 
         # When
-        actual_real_share_amount = graph.real_share_amount_for(query=entity_queried, focus=entity_in_focus)
+        actual_real_share_amount = graph.real_share_amount_for(query=entity_queried)
 
         # Then
         assert actual_real_share_amount == expected_real_share_amount
